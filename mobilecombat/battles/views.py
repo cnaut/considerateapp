cf04c9ab237682fcc62533cb765909ec6cd502bc
Charles Naut <cpn585@gmail.com>
2012-02-25 22:54:58 +0000

    Get request to add new user. Save data about user to database and return id of the user.

--- mobilecombat/battles/views.py
+++ mobilecombat/battles/views.py
@@ -4,15 +4,26 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.db import models 
 from battles.models import Battle
+from battles.models import User 
 
 def home(request):
 	return HttpResponse("Moble Combat Home")
 
 @csrf_exempt
+def adduser(request):
+	data = request.raw_post_data
+	data = json.loads(data)	
+	
+	user = User(name=data['name'], photo=request.FILES['image'])
+	user.save()
+	
+	return HttpResponse(user.id)
+
+@csrf_exempt
 def storetext(request):
 	data = request.raw_post_data
 	data = json.loads(data)	
-	battle = Battle.objects.create(text=data)
+	battle = Battle(text=data)
 	battle.save()
 	
 	print data['name']

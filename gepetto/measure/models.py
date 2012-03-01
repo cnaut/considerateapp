fa7b1ca37377dbfdd530bc36d666240cf5ab4338
EC2 Default User <ec2-user@ip-10-170-13-48.us-west-1.compute.internal>
2012-03-01 09:10:51 +0000

    Remove misnamed directory

--- gepetto/measure/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class TextMessage(models.Model):
-    text = models.TextField()
-    sender = models.TextField()
-    receiver = models.ListField()
-    timestamp = models.DateTimeField()
-    media = models.ImageField()
--- gepetto/measure/views.py
+++
@@ -1,5 +0,0 @@
-# Create your views here.
-from django.shortcuts import render_to_response
-
-def home(request):
-    return render_to_response('measure/home.html', {'greeting': 'hi!'})

2c80c905d65da949c6c099d2306ef3c238664c40
Charles Naut <cpn585@gmail.com>
2012-03-05 12:05:41 +0000

    Setup django-mongodb to work on Geppetto

--- geppetto/measure/models.py
+++ geppetto/measure/models.py
@@ -1,9 +1,9 @@
 from django.db import models
+from djangotoolbox.fields import ListField
 
 # Create your models here.
 class TextMessage(models.Model):
     text = models.TextField()
     sender = models.TextField()
-    receiver = models.ListField()
-    timestamp = models.DateTimeField()
-    media = models.ImageField()
+    receiver = ListField()
+    timestamp = models.DateTimeField(auto_now_add=True, null=True)

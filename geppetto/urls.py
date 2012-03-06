f476b27394b45db5f693c15cf8a53fd83f156acb
Charles Naut <cpn585@gmail.com>
2012-03-06 20:48:32 +0000

    Add url for location

--- geppetto/urls.py
+++ geppetto/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'measure.views.home', name='home'),
+    url(r'^battles/location', 'battles.views.location', name='location'),
     # url(r'^gepetto/', include('gepetto.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:

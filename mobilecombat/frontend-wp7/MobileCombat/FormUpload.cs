1c686456f45d55ff6aa876802eb76239efdfb628
U-Ben-PC\Ben <Ben@Ben-PC.(none)>
2012-03-06 23:58:32 -0800

    Recovered the C# mobile combat game from the decompiled XAP! Mostly working, just gonna check compatibility with the server.

---
+++ mobilecombat/frontend-wp7/MobileCombat/FormUpload.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace MobileCombat
+{
+    public static class FormUpload
+    {
+        private static string contentType;
+
+        private readonly static Encoding encoding;
+
+        private static string formDataBoundary;
+
+        static FormUpload()
+        {
+            FormUpload.encoding = Encoding.UTF8;
+            FormUpload.formDataBoundary = string.Format("----------{0:N}", Guid.NewGuid());
+            FormUpload.contentType = string.Concat("multipart/form-data; boundary=", FormUpload.formDataBoundary);
+        }
+
+        public static byte[] GetMultipartFormData(Dictionary<string, object> postParameters)
+        {
+            Stream formDataStream = new System.IO.MemoryStream();
+            bool needsCLRF = false;
+
+            foreach (var param in postParameters)
+            {
+                // Thanks to feedback from commenters, add a CRLF to allow multiple parameters to be added.
+                // Skip it on the first parameter, add it to subsequent parameters.
+                if (needsCLRF)
+                    formDataStream.Write(encoding.GetBytes("\r\n"), 0, encoding.GetByteCount("\r\n"));
+
+                needsCLRF = true;
+
+                if (param.Value is FileParameter)
+                {
+                    FileParameter fileToUpload = (FileParameter)param.Value;
+
+                    // Add just the first part of this param, since we will write the file data directly to the Stream
+                    string header = string.Format("--{0}\r\nContent-Disposition: form-data; name=\"{1}\"; filename=\"{2}\";\r\nContent-Type: {3}\r\n\r\n",
+                        formDataBoundary,
+                        param.Key,
+                        fileToUpload.FileName ?? param.Key,
+                        fileToUpload.ContentType ?? "application/octet-stream");
+
+                    formDataStream.Write(encoding.GetBytes(header), 0, encoding.GetByteCount(header));
+
+                    // Write the file data directly to the Stream, rather than serializing it to a string.
+                    formDataStream.Write(fileToUpload.File, 0, fileToUpload.File.Length);
+                }
+                else
+                {
+                    string postData = string.Format("--{0}\r\nContent-Disposition: form-data; name=\"{1}\"\r\n\r\n{2}",
+                        formDataBoundary,
+                        param.Key,
+                        param.Value);
+                    formDataStream.Write(encoding.GetBytes(postData), 0, encoding.GetByteCount(postData));
+                }
+            }
+
+            // Add the end of the request.  Start with a newline
+            string footer = "\r\n--" + formDataBoundary + "--\r\n";
+            formDataStream.Write(encoding.GetBytes(footer), 0, encoding.GetByteCount(footer));
+
+            // Dump the Stream into a byte[]
+            formDataStream.Position = 0;
+            byte[] formData = new byte[formDataStream.Length];
+            formDataStream.Read(formData, 0, formData.Length);
+            formDataStream.Close();
+
+            return formData;
+        }
+
+        public static HttpWebRequest GetRequest(string postUrl, string userAgent)
+        {
+            HttpWebRequest cookieContainer = WebRequest.Create(postUrl) as HttpWebRequest;
+            bool flag = cookieContainer != null;
+            if (flag)
+            {
+                cookieContainer.Method = "POST";
+                cookieContainer.ContentType = FormUpload.contentType;
+                cookieContainer.UserAgent = userAgent;
+                cookieContainer.CookieContainer = new CookieContainer();
+                HttpWebRequest httpWebRequest = cookieContainer;
+                return httpWebRequest;
+            }
+            else
+            {
+                throw new NullReferenceException("request is not a http request");
+            }
+        }
+
+        public class FileParameter
+        {
+            public string ContentType;
+
+            public byte[] File;
+
+            public string FileName;
+
+            public FileParameter(byte[] file, string filename, string contenttype)
+            {
+                this.File = file;
+                this.FileName = filename;
+                this.ContentType = contenttype;
+            }
+        }
+    }
+}
---
+++ mobilecombat/frontend-wp7/MobileCombat/GetPhoto.xaml.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.Phone.Controls;
+using System;
+using System.Windows.Controls;
+using Microsoft.Phone.Tasks;
+using System.Net;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Windows;
+using System.Diagnostics;
+using System.Windows.Media.Imaging;
+using System.Collections.Generic;
+
+namespace MobileCombat
+{
+    public partial class GetPhoto : PhoneApplicationPage
+    {
+        private byte[] formData;
+
+        private PhotoChooserTask photoChooser;
+
+        private byte[] picData;
+
+        private bool picReady;
+
+        public GetPhoto()
+        {
+            picReady = false;
+            InitializeComponent();
+            saveButton.IsEnabled = false;
+            photoChooser = new PhotoChooserTask();
+            photoChooser.PixelHeight = 180;
+            photoChooser.PixelWidth = 180;
+            photoChooser.ShowCamera = true;
+            photoChooser.Completed += photoChooserTask_Completed;
+        }
+
+        private void GotRequestStream(IAsyncResult asyncResult)
+        {
+            HttpWebRequest asyncState = asyncResult.AsyncState as HttpWebRequest;
+            Stream stream = asyncState.EndGetRequestStream(asyncResult);
+            stream.Write(formData, 0, (int)formData.Length);
+            stream.Flush();
+            stream.Close();
+            asyncState.BeginGetResponse(GotResponseStream, asyncState);
+        }
+
+        private void GotResponseStream(IAsyncResult asyncResult)
+        {
+            HttpWebRequest asyncState = asyncResult.AsyncState as HttpWebRequest;
+            HttpWebResponse httpWebResponse = asyncState.EndGetResponse(asyncResult) as HttpWebResponse;
+            Stream responseStream = httpWebResponse.GetResponseStream();
+            StreamReader streamReader = new StreamReader(responseStream);
+            string end = streamReader.ReadToEnd();
+            IsolatedStorageSettings applicationSettings = IsolatedStorageSettings.ApplicationSettings;
+            applicationSettings["id"] = end;
+            Deployment.Current.Dispatcher.BeginInvoke(new Action(base.NavigationService.GoBack));
+        }
+
+        private void nameBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            bool flag;
+            bool text = !(nameBox.Text == string.Empty);
+            if (!text)
+            {
+                saveButton.IsEnabled = false;
+            }
+            if (nameBox.Text == string.Empty)
+            {
+                flag = true;
+            }
+            else
+            {
+                flag = !picReady;
+            }
+            text = flag;
+            if (!text)
+            {
+                saveButton.IsEnabled = true;
+            }
+        }
+
+        private void photoChooserTask_Completed(object sender, PhotoResult e)
+        {
+            bool taskResult = e.TaskResult != TaskResult.OK;
+            if (!taskResult)
+            {
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.SetSource(e.ChosenPhoto);
+                userImg.Source = bitmapImage;
+                WriteableBitmap writeableBitmap = new WriteableBitmap(bitmapImage);
+                MemoryStream memoryStream = new MemoryStream();
+                try
+                {
+                    Extensions.SaveJpeg(writeableBitmap, memoryStream, bitmapImage.PixelHeight, bitmapImage.PixelWidth, 0, 100);
+                    picData = memoryStream.ToArray();
+                }
+                finally
+                {
+                    taskResult = memoryStream == null;
+                    if (!taskResult)
+                    {
+                        memoryStream.Dispose();
+                    }
+                }
+                picReady = true;
+                taskResult = !(nameBox.Text != string.Empty);
+                if (!taskResult)
+                {
+                    saveButton.IsEnabled = true;
+                }
+            }
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Dictionary<string, object> strs = new Dictionary<string, object>();
+            strs.Add("name", nameBox.Text);
+            strs.Add("photo", new FormUpload.FileParameter(picData, "file.jpg", "application/octet-stream"));
+            formData = FormUpload.GetMultipartFormData(strs);
+            HttpWebRequest request = FormUpload.GetRequest("http://184.169.136.30:8000/adduser", "");
+            request.BeginGetRequestStream(GotRequestStream, request);
+        }
+
+        private void selectPicButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                photoChooser.Show();
+            }
+            catch (InvalidOperationException invalidOperationException1)
+            {
+                InvalidOperationException invalidOperationException = invalidOperationException1;
+                MessageBox.Show("An error occurred.");
+            }
+        }
+    }
+}

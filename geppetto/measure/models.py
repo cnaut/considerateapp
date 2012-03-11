ba07cf131cea85d17f891a5e0c3ff951f896b1cf
Charles Naut <cpn585@gmail.com>
2012-03-11 20:56:48 +0000

    Add user and activity models for measure part of gepetto framework

--- geppetto/measure/models.py
+++ geppetto/measure/models.py
@@ -3,7 +3,19 @@ from djangotoolbox.fields import ListField
 
 # Create your models here.
 class TextMessage(models.Model):
-    text = models.TextField()
-    sender = models.TextField()
-    receiver = ListField()
-    timestamp = models.DateTimeField(auto_now_add=True, null=True)
+	text = models.TextField()
+    	sender = models.TextField()
+    	receiver = ListField()
+    	timestamp = models.DateTimeField(auto_now_add=True, null=True)
+
+class User(models.Model):
+	name = models.TextField()
+	photo = models.FileField(upload_to='user_photos') 
+	joined_on = models.DateTimeField(auto_now_add=True, null=True)
+
+class Activity(models.Model):
+	activity = models.TextField()	
+	users = ListField()
+	lat = models.DecimalField(max_digits=8, decimal_places=4, null=True)
+	long = models.DecimalField(max_digits=8, decimal_places=4, null=True)
+	start_time = models.DateTimeField(auto_now_add=True, null=True)

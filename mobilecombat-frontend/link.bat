ce74e6ef924c183dc08dd7d6df9f80837b4b5670
Ben Goldsmith <bengold@stanford.edu>
2012-03-14 17:09:38 -0700

    added the android side to the link.bat script.

--- mobilecombat-frontend/link.bat
+++ mobilecombat-frontend/link.bat
@@ -1,3 +1,4 @@
 @echo off
 mklink /D "%~dp0/frontend-wp7\mobilecombat-phonegap\www" "..\..\phonegap-src"
+mklink /D "%~dp0/frontend-android\MobileCombatAndroid\assets\www" "..\..\..\phonegap-src"
 PAUSE

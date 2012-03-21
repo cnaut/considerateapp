e381f7da2764f10fbbd6d05249b90d858250bb61
Charles Naut <cpn585@gmail.com>
2012-03-21 05:33:22 +0000

    Comment functions in views

--- geppetto/battles/views.py
+++ geppetto/battles/views.py
@@ -15,15 +15,7 @@ from django.dispatch import receiver
 def home(request):
 	return HttpResponse("Moble Combat Home")
 
-@csrf_exempt
-def allusers(request):
-	users = User.objects.all()
-	users = serializers.serialize("json", users)
-	
-	response = HttpResponse(users)
-	response['Cache-Control'] = 'no-cache'
-	return response
-
+#Takes a name and a photo and adds user to the database then returns user id 
 @csrf_exempt
 def adduser(request):
 	data = None
@@ -36,22 +28,13 @@ def adduser(request):
 		data = json.loads(data)
 		name = data['name']	
 		
+
 	user = User(name=name, photo=request.FILES['photo'])
-	print user.photo
 	user.save()
 	
 	return HttpResponse(user.id)
 
-
-@csrf_exempt
-def userform(request):
-	form = UserForm() 	
-	return render_to_response(
-		'userform.html',
-		{'form': form},
-		context_instance=RequestContext(request)
-	)
-
+#Takes list of users and creates battle then returns battle id
 def startbattle(request):
 	data = request.raw_post_data
 	data = json.loads(data)
@@ -64,35 +47,23 @@ def startbattle(request):
 	battle.save()
 	return HttpResponse(battle.id)
 
+#Takes user id and returns latest battle that user was in - still not done
 @csrf_exempt
 def getbattle(request):
 	id = None
 	if(request.POST.get('id')):
-		id = data.get('id')
+		id = request.POST.get('id')
 	else:		
-		data = request.raw_post_data
-		data = json.loads(data)
-		id = data['id']	
+		id = request.GET.get('id')	
 		
-	battle = Battle.objects.order_by("checkin_time").get(users=id)[:1]	
+	battle = Battle.objects.order_by("checkin_time").get()[0:1]	
 	return HttpResponse(battle.id)
 
-
+#Takes a battle id and a user id and adds user id to list of losers for battle 
 @csrf_exempt
 def declaredefeat(request):
 	data = request.GET
 	battle = Battle.objects.get(id=data.get('battleid'))	
 	battle.losers.extend([data.get('userid')]) 
 	battle.save()
-	print battle.losers
 	return HttpResponse(battle.losers)
-
-
-
-@csrf_exempt
-def location(request):
-	lat = request.POST.get('lat')
-	long = request.POST.get('long')
-	battle = Battle(users=[], losers=[], lat=lat, long=long)
-	battle.save()
-	return HttpResponse(battle.id)

5ba2d3b1cb167c252a3742ccbde519434489c9aa
Ben Goldsmith <bengold@stanford.edu>
2012-04-10 20:47:01 +0000

    Fixed tabs and spacing and added wsgi.

--- geppetto/battles/views.py
+++ geppetto/battles/views.py
@@ -1,101 +1,101 @@
 import json
-import django.dispatch 
+import django.dispatch
 
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
-from django.db import models 
+from django.db import models
 from django.core import serializers
 from battles.models import Battle
-from battles.models import User 
+from battles.models import User
 from battles.forms import UserForm
 from django.dispatch import receiver
 
 def home(request):
-	return HttpResponse("Moble Combat Home")
+    return HttpResponse("Moble Combat Home")
 
 @csrf_exempt
 def allusers(request):
-	users = User.objects.all()
-	users = serializers.serialize("json", users)
-	
-	response = HttpResponse(users)
-	response['Cache-Control'] = 'no-cache'
-	return response
+    users = User.objects.all()
+    users = serializers.serialize("json", users)
+
+    response = HttpResponse(users)
+    response['Cache-Control'] = 'no-cache'
+    return response
 
 @csrf_exempt
 def adduser(request):
-	data = None
-	name = None
-	if(request.POST.get('name')):
-		data = request.POST
-		name = data.get('name')
-	else:		
-		data = request.raw_post_data
-		data = json.loads(data)
-		name = data['name']	
-		
-	user = User(name=name, photo=request.FILES['photo'])
-	print user.photo
-	user.save()
-	
-	return HttpResponse(user.id)
+    data = None
+    name = None
+    if(request.POST.get('name')):
+        data = request.POST
+        name = data.get('name')
+    else:
+        data = request.raw_post_data
+        data = json.loads(data)
+        name = data['name']
+
+    user = User(name=name, photo=request.FILES['photo'])
+    print user.photo
+    user.save()
+
+    return HttpResponse(user.id)
 
 
 @csrf_exempt
 def userform(request):
-	form = UserForm() 	
-	return render_to_response(
-		'userform.html',
-		{'form': form},
-		context_instance=RequestContext(request)
-	)
+    form = UserForm()
+    return render_to_response(
+        'userform.html',
+        {'form': form},
+        context_instance=RequestContext(request)
+    )
 
 def startbattle(request):
-	data = request.raw_post_data
-	data = json.loads(data)
-	
- 	users = []
-	for user in data['users']:
-		users.append(user.encode("ascii"))
-	battle = Battle(users=users, losers=[])
-	print battle
-	battle.save()
-	return HttpResponse(battle.id)
+    data = request.raw_post_data
+    data = json.loads(data)
+
+    users = []
+    for user in data['users']:
+        users.append(user.encode("ascii"))
+    battle = Battle(users=users, losers=[])
+    print battle
+    battle.save()
+    return HttpResponse(battle.id)
 
 @csrf_exempt
 def getbattle(request):
-	id = None
-	if(request.POST.get('id')):
-		id = data.get('id')
-	else:		
-		data = request.raw_post_data
-		data = json.loads(data)
-		id = data['id']	
-    
-	battle = Battle.objects.order_by("checkin_time").get(users=id)[:1]	
-  if(battle.get('checkout_time') != null):
-    battle = null
-  
-	return HttpResponse(battle.id)
+    id = None
+    if(request.POST.get('id')):
+        id = data.get('id')
+    else:
+        data = request.raw_post_data
+        data = json.loads(data)
+        id = data['id']
+
+    battle = Battle.objects.order_by("checkin_time").get(users=id)[:1]
+    if(battle.get('checkout_time') != null):
+        battle = null
+
+    return HttpResponse(battle.id)
 
 
 @csrf_exempt
 def declaredefeat(request):
-	data = request.GET
-	battle = Battle.objects.get(id=data.get('battleid'))
-	battle.losers.extend([data.get('userid')]) 
-	battle.save()
-	print battle.losers
-	return HttpResponse(battle.losers)
+    data = request.GET
+    battle = Battle.objects.get(id=data.get('battleid'))
+    battle.losers.extend([data.get('userid')])
+    battle.save()
+    print battle.losers
+    return HttpResponse(battle.losers)
 
 
 
 @csrf_exempt
 def location(request):
-	lat = request.POST.get('lat')
-	long = request.POST.get('long')
-	battle = Battle(users=[], losers=[], lat=lat, long=long)
-	battle.save()
-	return HttpResponse(battle.id)
+    lat = request.POST.get('lat')
+    long = request.POST.get('long')
+    battle = Battle(users=[], losers=[], lat=lat, long=long)
+    battle.save()
+    return HttpResponse(battle.id)
--- geppetto/settings.py
+++ geppetto/settings.py
@@ -109,9 +109,9 @@ SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.	
+    # Don't forget to use absolute paths, not relative paths.
     #'/home/ec2-user/geppetto/templates/',
-    os.path.join(SITE_ROOT, 'templates'), 
+    os.path.join(SITE_ROOT, 'templates'),
 )
 
 INSTALLED_APPS = (

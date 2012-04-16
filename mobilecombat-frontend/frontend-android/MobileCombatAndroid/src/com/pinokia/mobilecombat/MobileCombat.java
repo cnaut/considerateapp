9975a9076a285ec60fc63a764630a544a9e5a0aa
Charles Naut <cpn585@gmail.com>
2012-04-16 01:36:17 -0700

    Rename android application

---
+++ mobilecombat-frontend/frontend-android/MobileCombatAndroid/src/com/pinokia/mobilecombat/MobileCombat.java
@@ -0,0 +1,15 @@
+package com.pinokia.mobilecombat;
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+public class MobileCombat extends DroidGap
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}

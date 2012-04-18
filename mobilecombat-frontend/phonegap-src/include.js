320af903a8a09853220c20af2aed72582e63575a
Charles Naut <cpn585@gmail.com>
2012-04-18 06:36:19 -0700

    Restart android applicaiton, change code for android, and release apk

---
+++ mobilecombat-frontend/phonegap-src/include.js
@@ -0,0 +1,11 @@
+﻿﻿if (navigator.userAgent.toLowerCase().match(/android/)) {
+	document.write("<script charset='utf-8' src='android\/cordova-1.5.0.js'><\/script>");
+	document.write("<script type='text\/javascript' charset='utf-8' src='android\/cdv-plugin-fb-connect.js'><\/script>");
+	document.write("<script type='text\/javascript' charset='utf-8' src='facebook_js_sdk.js'><\/script>");
+   
+} else if (navigator.userAgent.toLowerCase().match(/windows/)) {
+	document.write("<script type='text\/javascript' charset='utf-8' src='wp\/cordova-1.5.0.js'><\/script>");
+	document.write("<script type='text\/javascript' charset='utf-8' src='wp\/pg-plugin-fb-connect.js'><\/script>");
+	document.write("<script type='text\/javascript' charset='utf-8' src='facebook_js_sdk.js'><\/script>");
+	document.write("<script type='text\/javascript' charset='utf-8' src='wp\/ChildBrowser.js'><\/script>");
+}

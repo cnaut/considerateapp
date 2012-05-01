b12fb3c6ced0861c551fd5df97c8d25522c05954
Charles Naut <cpn585@gmail.com>
2012-05-01 03:24:34 -0700

    IntentService now listens for phone flip to silence phone as a background process

--- considerateapp-frontend/src/com/pinokia/considerateapp/ConsiderateAppActivity.java
+++ considerateapp-frontend/src/com/pinokia/considerateapp/ConsiderateAppActivity.java
@@ -1,77 +1,26 @@
 package com.pinokia.considerateapp;
 
 import java.util.List;
-
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.AudioManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
-public class ConsiderateAppActivity extends Activity implements SensorEventListener{
-    private SensorManager sensorManager;
-    private Sensor accelerometerSensor;
-    private AudioManager am;
-    private PrevState pv;
-    TextView flippedText;
-    boolean accelerometerPresent;
-    
-    //State to recover
-    private class PrevState {
-      int audioState;
-    }
-    
+public class ConsiderateAppActivity extends Activity {
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-    	flippedText = (TextView)findViewById(R.id.textView1);
-    	flippedText.setText("Not flipped");
-     	
-    	sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-        accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
-        
-        List<Sensor> sensorList = sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER);
-        if(sensorList.size() > 0){
-         accelerometerPresent = true;
-         accelerometerSensor = sensorList.get(0);  
-        }
-        else{
-         accelerometerPresent = false;  
-         flippedText.setText("No accelerometer present!");
-        }
-        am = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
-        pv = new PrevState();
-        pv.audioState = am.getRingerMode();
+    	super.onCreate(savedInstanceState);
+	    setContentView(R.layout.main);
+	    Intent flipIntent = new Intent(getApplicationContext(), FlipIntentService.class);
+        startService(flipIntent);
     }
-
-	@Override
-	public void onAccuracyChanged(Sensor sensor, int accuracy) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-  @Override
-	public void onSensorChanged(SensorEvent event) {
-    
-		float z_value = event.values[2];
-		if (z_value >= 0){
-		  am.setRingerMode(pv.audioState);
-			flippedText.setText("Face UP");
-		}
-		else{
-		  //Turn phone to Silent mode
-		  am.setRingerMode(AudioManager.RINGER_MODE_SILENT);
-		  
-			flippedText.setText("Face down");
-		}
-	}
-	
 }

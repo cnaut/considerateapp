bd944ca67884dbd59ae1d1b74292098801d64b36
Charles Naut <cpn585@gmail.com>
2012-05-27 23:46:50 +0000

    Setup django's automatic admin interface

--- considerateapp/urls.py
+++ considerateapp/urls.py
@@ -2,14 +2,14 @@ from django.conf.urls.defaults import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin 
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'phonedata.views.home', name='home'),
     url(r'^profile$', 'phonedata.views.profile', name='profile'),
-    url(r'^admin$', 'phonedata.views.admin', name='admin'),
+    #url(r'^admin$', 'phonedata.views.admin', name='admin'),
     url(r'^channel$', 'phonedata.views.channel', name='channel'),
     url(r'^adduser', 'phonedata.views.adduser', name='adduser'),
     url(r'^allusers', 'phonedata.views.allusers', name='alluser'),
@@ -26,6 +26,6 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
 urlpatterns += staticfiles_urlpatterns()

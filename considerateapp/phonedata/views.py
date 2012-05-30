e15885b08a9af07878c76aa01d0e092a87b9eacd
Charles Naut <cpn585@gmail.com>
2012-05-30 03:25:29 +0000

    Add features to view all users and see stats for each user

--- considerateapp/phonedata/views.py
+++ considerateapp/phonedata/views.py
@@ -18,9 +18,13 @@ def home(request):
 def profile(request):
     return render_to_response('profile.html')
 
-def admin(request):
-    stats = Stat.objects.all().order_by('-value')[0:100]
-    return render_to_response('admin.html', {'stats': stats}, context_instance=RequestContext(request))
+def dataview(request):
+    users = User.objects.all()
+    return render_to_response('admin.html', {'users': users}, context_instance=RequestContext(request))
+
+def userstats(request, phoneid):
+    stats = Stat.objects.filter(user=phoneid).order_by('type')
+    return render_to_response('userstats.html', {'id': phoneid, 'stats': stats}, context_instance=RequestContext(request))
 
 def channel(request):
    return HttpResponse('<script src="//connect.facebook.net/en_US/all.js"></script>')
